e6099cb3e50bcbad093b530d1fdedc4cd87a473d
S Varez <varezsergio@gmail.com>
2021-08-31 11:53:10 +0200

    Creation of the Quiz component

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import { quizConstructor } from '../utils/quizConstructor'
 
 export const Quiz = () => {
+
+    const { title,  answers } = quizConstructor
+
     return (
-        <div>
+        <>
+            <div className="question">
+                <div className="question__title">
+                    <h1>{ title }</h1>
+                </div>
+
+                <div className="question__answers">
+
+                    {
+                        Object.keys(answers).map(id=>
+                            <label className="radio radio-gradient" key={answers[id]}>
+                                <span className="radio__input">
+                                    <input
+                                        
+                                        name='answer'
+                                        type="radio"
+                                    /> 
+                                <span className="radio__control"></span>
+                                </span>
+                                <span className="radio__label">{answers[id]}</span>
+                            </label>
+                        )
+                    }
+
+                </div>
+
+            </div>
             
-        </div>
+        </>
     )
 }
---
+++ src/utils/quizConstructor.js
@@ -0,0 +1,12 @@
+export const quizConstructor = 
+    {
+        title: 'Who was the director of the godfather?',
+        answers: 
+            {
+                0: 'Martin Scorsese', 
+                1: 'Francis Ford Coppola', 
+                2: 'Stanley Kubrick', 
+                3: 'Steven Spielberg'
+            },
+        correctAnswer: 1
+    }
